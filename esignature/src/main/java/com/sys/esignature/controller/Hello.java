51f0d878621549216f85b4e002980cc63b1169a9
wjiaying <wjiaying@neusoft.com>
2024-02-14 22:17:50 +0800

    修改后端路径
    统一使用/api Hello类作为后端测试类

--- esignature/src/main/java/com/sys/esignature/controller/Hello.java
+++ esignature/src/main/java/com/sys/esignature/controller/Hello.java
@@ -1,12 +1,13 @@
 package com.sys.esignature.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
-
+@CrossOrigin
+@RequestMapping("/api/hello")
 public class Hello {
     @GetMapping("/index")
     public String index() {
