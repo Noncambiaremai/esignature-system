81424474b57e65961a9fd43cefa03757c43d72d1
wjiaying <wjiaying@neusoft.com>
2024-02-21 15:40:27 +0800

    添加注解

--- esignature/src/main/java/com/sys/esignature/EsignatureApplication.java
+++ esignature/src/main/java/com/sys/esignature/EsignatureApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sys")
 public class EsignatureApplication {
 
     public static void main(String[] args) {
