70dda30f9da1ef35dfe5fa71421aa94a07a91f69
wjiaying <wjiaying@neusoft.com>
2024-03-03 17:17:42 +0800

    前端将menu封装成组件
    每个前端页面都可以使用

--- vue_sys/src/components/menu.vue
+++ vue_sys/src/components/menu.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
     <el-container style="width: 100vw; height: 100vh; border: 1px solid #eee">
-      <el-aside width="200px;" style="background-color: rgb(238, 241, 246); ">
+      <el-aside width="200px;">
+        <!--style="background-color: rgb(238, 241, 246); "-->
         <el-menu style="height: 100%;" :default-openeds="['1', '3']">
-          <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-1" @click="gotologin">选项1</el-menu-item>
               <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
+
             <el-menu-item-group title="分组2">
               <el-menu-item index="1-3">选项3</el-menu-item>
             </el-menu-item-group>
@@ -17,7 +14,6 @@
               <template slot="title">选项4</template>
               <el-menu-item index="1-4-1">选项4-1</el-menu-item>
             </el-submenu>
-          </el-submenu>
           <el-submenu index="2">
             <template slot="title"><i class="el-icon-menu"></i>导航二</template>
             <el-menu-item-group>
@@ -65,15 +61,9 @@
         </el-header>
 
         <el-main>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="日期" width="140">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
-          </el-table>
+          <slot></slot>
         </el-main>
+
       </el-container>
     </el-container>
   </div>
@@ -82,18 +72,21 @@
 
 <style>
   .el-header {
-    background-color: #B3C0D1;
+    background-color: #b6c9b8;
     color: #333;
     line-height: 60px;
   }
 
   .el-aside {
-    color: #333;
+    /*color: #4d785c;*/
   }
 </style>
 
 <script>
-  export default {
+import axios from 'axios';
+
+export default {
+    name: "menu",
     data() {
       const item = {
         date: '2016-05-02',
@@ -102,6 +95,11 @@
       };
       return {
         tableData: Array(20).fill(item)
+      }
+    },
+    methods: {
+      gotologin() {
+        this.$router.push({name: 'login'});
       }
     }
   };
