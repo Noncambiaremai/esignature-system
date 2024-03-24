92fada01b7550c44ff0bdf4588e441cf55f2b385
wjiaying <wjiaying@neusoft.com>
2024-03-24 19:41:11 +0800

    前端修改人脸识别界面 调整函数顺序
    分成多个方法实现不同功能 代码更清晰

--- vue_sys/src/components/test.vue
+++ vue_sys/src/components/test.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="container">
     <video ref="videoElement" class="input_video"></video>
-    <canvas class="output_canvas" width="660px" height="480px"></canvas>
+    <canvas class="output_canvas" width="850px" height="480px"></canvas>
   </div>
 </template>
 
 <script>
-  // 在这里导入 Mediapipe 相关的库
-  // import * as mpCameraUtils from '@mediapipe/camera_utils/camera_utils.js';
-  // import * as mpControlUtils from '@mediapipe/control_utils/control_utils.js';
-  // import * as mpDrawingUtils from '@mediapipe/drawing_utils/drawing_utils.js';
-  // import * as mpFaceMesh from '@mediapipe/face_mesh/face_mesh.js';
   import { FaceMesh, FACEMESH_TESSELATION, FACEMESH_RIGHT_EYE,
     FACEMESH_RIGHT_EYEBROW, FACEMESH_RIGHT_IRIS, FACEMESH_LEFT_EYE,
     FACEMESH_LEFT_EYEBROW, FACEMESH_LEFT_IRIS, FACEMESH_FACE_OVAL,
@@ -18,70 +13,79 @@
   import { Camera } from '@mediapipe/camera_utils/camera_utils.js';
   import { drawConnectors } from '@mediapipe/drawing_utils/drawing_utils.js';
 
-    export default {
-        name: "test",
-        methods: {
-        },
-      mounted() {
-        const videoElement = document.getElementsByClassName('input_video')[0];
-        const canvasElement = document.getElementsByClassName('output_canvas')[0];
-        const canvasCtx = canvasElement.getContext('2d');
+  export default {
+    name: "test",
+    data() {
+      return {
+        videoElement: null,
+        canvasElement: null,
+        canvasCtx: null,
+        faceMesh: null,
+        camera: null
+      };
+    },
+    methods: {
+      initFaceMesh() {
+        this.videoElement = document.getElementsByClassName('input_video')[0];
+        this.canvasElement = document.getElementsByClassName('output_canvas')[0];
+        this.canvasCtx = this.canvasElement.getContext('2d');
 
-        function onResults(results) {
-          canvasCtx.save();
-          canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-          canvasCtx.drawImage(
-            results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-          if (results.multiFaceLandmarks) {
-            for (const landmarks of results.multiFaceLandmarks) {
-              drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
-                {color: '#C0C0C070', lineWidth: 1});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
-              drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
-            }
-          }
-          canvasCtx.restore();
-        }
-
-        const faceMesh = new FaceMesh({locateFile: (file) => {
+        this.faceMesh = new FaceMesh({locateFile: (file) => {
             return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
           }});
-        faceMesh.setOptions({
+        this.faceMesh.setOptions({
           maxNumFaces: 1,
           refineLandmarks: true,
           minDetectionConfidence: 0.5,
           minTrackingConfidence: 0.5
         });
 
-        console.log("huhuhu");
-
-        faceMesh.onResults(onResults);
-
-        const camera = new Camera(videoElement, {
+        this.faceMesh.onResults(this.onResults);
+      },
+      startFaceDetection() {
+        this.camera = new Camera(this.videoElement, {
           onFrame: async () => {
-            await faceMesh.send({image: videoElement});
+            await this.faceMesh.send({image: this.videoElement});
           },
-          width: 1280,
-          height: 720
+          width: 850,
+          height: 480
         });
-        camera.start();
+        this.camera.start();
+      },
+      onResults(results) {
+        this.canvasCtx.save();
+        this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+        this.canvasCtx.drawImage(
+          results.image, 0, 0, this.canvasElement.width, this.canvasElement.height);
 
+        if (results.multiFaceLandmarks) {
+          for (const landmarks of results.multiFaceLandmarks) {
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_TESSELATION,
+              {color: '#C0C0C070', lineWidth: 1});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+          }
+        }
+        this.canvasCtx.restore();
       }
-    }
+    },
+    mounted() {
+      this.initFaceMesh();
+      this.startFaceDetection();
+    },
+  }
 </script>
 
+
+
 <style scoped>
-  .input_video, .output_canvas {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  .input_video {
+    display: none;
   }
 </style>
