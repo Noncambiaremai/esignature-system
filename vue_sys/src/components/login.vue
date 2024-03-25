05a19ac6dd87276f0dd3e94ae345b781e2843204
wjiaying <wjiaying@neusoft.com>
2024-03-25 14:43:50 +0800

    前端修改登录页面
    精简登录页面 把不该要的地方全部删除了

--- vue_sys/src/components/login.vue
+++ vue_sys/src/components/login.vue
@@ -1,36 +1,6 @@
 <template>
   <div class="login">
-    <el-form v-if="enrollflag" ref="enrollForm" class="enroll-form">
-      <h3 class="title">新用户注册</h3>
-      <el-form-item prop="userId">
-        <el-input placeholder="手机号">
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
-        </el-input>
-
-      </el-form-item>
-      <el-form-item prop="userPassword">
-        <el-input placeholder="密码">
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-
-      <el-form-item>
-        <el-button style="width: 400px" @click="turnCamera">打开 / 关闭摄像头</el-button>
-      </el-form-item>
-      
-      <el-form-item v-if="cameraVisible" label="摄像头">
-        <video ref="video" style="width: 100%; height: 100%;" autoplay></video>
-        <!--<canvas ref="canvas" width="640" height="480"></canvas>-->
-      </el-form-item>
-
-
-      <el-form-item style="width:100%;">
-        <el-button size="medium" type="primary" style="width: 150px">注 册</el-button>
-        <el-button size="medium" type="primary" style="width: 150px" @click="goback">返 回</el-button>
-      </el-form-item>
-    </el-form>
-
-    <el-form v-if="!enrollflag" ref="loginForm" class="login-form">
+    <el-form ref="loginForm" class="login-form">
       <h3 class="title">欢迎登录手势识别电子签名系统</h3>
       <el-form-item prop="userId">
         <el-input placeholder="手机号">
@@ -44,7 +14,7 @@
       </el-form-item>
 
       <el-form-item style="width:100%;">
-        <el-button size="medium" type="primary" style="width: 150px" @click="tologin">登 录</el-button>
+        <el-button size="medium" type="primary" style="width: 150px" >登 录</el-button>
         <el-button size="medium" type="primary" style="width: 150px" @click="toenroll">注 册</el-button>
 
         <el-button size="medium" type="primary"
@@ -57,79 +27,23 @@
 
 <script>
 import axios from 'axios';
-import * as tf from '@tensorflow/tfjs';
-import * as mediapipe from '@mediapipe/face_mesh';
 
 export default {
     name: "login",
-    data() {
-      return {
-        enrollflag: false,
-        cameraVisible: false,
-        video: null,
-        stream: null,
-        canvas: null,
-        ctx: null,
-        faceMesh: null,
-      };
-    },
-    created(){
-      this.enrollflag = false;
-    },
-
     methods: {
       // 注册按钮跳转
       toenroll(){
-        this.enrollflag = true;
-      },
-      tologin(){
         this.$router.push({name: 'test'});
       },
-
-      goback(){
-        this.enrollflag = false;
-      },
-
       gotomenu() {
         this.$router.push({name: 'menu'});
-      },
-
-      turnCamera() {
-        this.cameraVisible = !this.cameraVisible;
-        if (this.cameraVisible) {
-          // this.canvas = this.$refs.canvas;
-          // this.ctx = this.canvas.getContext('2d');
-
-          navigator.mediaDevices.getUserMedia({video: true})
-            .then((stream) => {
-              this.stream = stream;
-              this.video = this.$refs.video;
-              this.video.srcObject = stream;
-              this.video.play();
-            })
-            .catch((error) => {
-              console.error('Error accessing the camera: ', error);
-            });
-
-          // this.initializeMediaPipe();
-          // this.detectFaces();
-        }
-        else {
-          const tracks = this.stream.getTracks();
-          tracks.forEach(track => track.stop());
-          // 清空视频元素的 srcObject
-          this.video.srcObject = null;
-        }
-      },
-
       }
-
+      }
             // callbackend() {
             //   axios.get('/api/hello/index').then(response => {
             //     console.log(response.data);
             //   });
             // }
-
     }
 </script>
 
