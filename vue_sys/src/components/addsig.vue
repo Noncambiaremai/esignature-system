2784dc007b563277cc5574b6548c9a90feafb8da
wjiaying <wjiaying@neusoft.com>
2024-03-27 15:55:00 +0800

    前端修改添加签名页面
    规范化代码 方法模块化 数据返回this.data

--- vue_sys/src/components/addsig.vue
+++ vue_sys/src/components/addsig.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Menu>
-      <div class="container">
+      <el-button style="width: 400px" @click="toggleCamera">打开 / 关闭摄像头</el-button>
+      <div class="container" v-show="cameraVisible">
         <video class="input_video"></video>
         <canvas class="output_canvas" width="400px" height="300px"></canvas>
       </div>
@@ -10,59 +11,91 @@
 </template>
 
 <script>
-import Menu from "@/components/menu.vue";
-import { Camera } from '@mediapipe/camera_utils/camera_utils.js';
-import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils/drawing_utils.js';
-import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands/hands.js';
+  import Menu from "@/components/menu.vue";
+  import { Camera } from '@mediapipe/camera_utils/camera_utils.js';
+  import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils/drawing_utils.js';
+  import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands/hands.js';
 
-    export default {
-        name: "addsig",
-        components: {
-          Menu,
-        },
-        mounted() {
-          const videoElement = document.getElementsByClassName('input_video')[0];
-          const canvasElement = document.getElementsByClassName('output_canvas')[0];
-          const canvasCtx = canvasElement.getContext('2d');
+  export default {
+    name: "addsig",
+    components: {
+      Menu,
+    },
+    data() {
+      return {
+        videoElement: null,
+        canvasElement: null,
+        canvasCtx: null,
+        hands: null,
+        camera: null,
+        cameraVisible: false,
+      };
+    },
+    methods: {
+      initHands() {
+        this.videoElement = document.getElementsByClassName('input_video')[0];
+        this.canvasElement = document.getElementsByClassName('output_canvas')[0];
+        this.canvasCtx = this.canvasElement.getContext('2d');
 
-          function onResults(results) {
-            canvasCtx.save();
-            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-            canvasCtx.drawImage(
-              results.image, 0, 0, canvasElement.width, canvasElement.height);
-            if (results.multiHandLandmarks) {
-              for (const landmarks of results.multiHandLandmarks) {
-                drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                  {color: '#00FF00', lineWidth: 5});
-                drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
-              }
-            }
-            canvasCtx.restore();
+        this.hands = new Hands({
+          locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
           }
+        });
+        this.hands.setOptions({
+          maxNumHands: 2,
+          modelComplexity: 1,
+          minDetectionConfidence: 0.5,
+          minTrackingConfidence: 0.5
+        });
 
-          const hands = new Hands({locateFile: (file) => {
-              return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-            }});
-          hands.setOptions({
-            maxNumHands: 2,
-            modelComplexity: 1,
-            minDetectionConfidence: 0.5,
-            minTrackingConfidence: 0.5
-          });
-          hands.onResults(onResults);
+        this.hands.onResults(this.onResults);
+      },
+      startHandDetection() {
+        this.camera = new Camera(this.videoElement, {
+          onFrame: async () => {
+            await this.hands.send({ image: this.videoElement });
+          },
+          // width: 1280,
+          // height: 720
+        });
+        this.camera.start();
+      },
+      onResults(results) {
+        this.canvasCtx.save();
+        this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+        this.canvasCtx.drawImage(
+          results.image, 0, 0, this.canvasElement.width, this.canvasElement.height);
 
-          const camera = new Camera(videoElement, {
-            onFrame: async () => {
-              await hands.send({image: videoElement});
-            },
-            width: 1280,
-            height: 720
-          });
-          camera.start();
-
+        if (results.multiHandLandmarks) {
+          for (const landmarks of results.multiHandLandmarks) {
+            drawConnectors(this.canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
+            drawLandmarks(this.canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+          }
         }
+        this.canvasCtx.restore();
+      },
+      toggleCamera() {
+        this.cameraVisible = !this.cameraVisible;
+        if (this.cameraVisible) {
+          this.startHandDetection();
+        } else {
+          this.stopHandDetection();
+        }
+      },
+      stopHandDetection() {
+        if (this.camera) {
+          this.camera.stop();
+          this.camera = null;
+        }
+      }
+    },
+    mounted() {
+      this.initHands();
     }
+  }
 </script>
+
 
 <style scoped>
   .input_video {
