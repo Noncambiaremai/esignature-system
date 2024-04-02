eac06bcb9c92423834aea949f6bd4194286434d8
wjiaying <wjiaying@neusoft.com>
2024-04-02 22:16:20 +0800

    前端添加签名页面去掉没用的注释

--- vue_sys/src/components/adddoc.vue
+++ vue_sys/src/components/adddoc.vue
@@ -27,12 +27,7 @@ import Menu from "@/components/menu.vue";
         },
         data() {
           return {
-            fileList: [
-              // {
-              //   name: 'food.jpeg',
-              //   url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-              // },
-            ]
+            fileList: []
           }
         },
         methods: {
