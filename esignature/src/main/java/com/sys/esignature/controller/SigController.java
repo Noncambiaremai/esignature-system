d30a5fe859dc06eec33028a51a1511d53ef59a2f
wjiaying <wjiaying@neusoft.com>
2024-04-03 15:32:01 +0800

    后端实现签名上传功能
    基本实现签名信息上传数据库； 签名图像保存到本地文件夹；

--- esignature/src/main/java/com/sys/esignature/controller/SigController.java
+++ esignature/src/main/java/com/sys/esignature/controller/SigController.java
@@ -1,19 +1,21 @@
 package com.sys.esignature.controller;
 
+import org.apache.commons.io.FileUtils;
 import com.sys.esignature.service.SigService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/sig")
 public class SigController {
     private static final String UPLOAD_PATH = "C:/Users/lenovo/Desktop/undergraduate_design/images";
@@ -22,47 +24,40 @@ public class SigController {
     private SigService sigService;
 
     @CrossOrigin(origins = "http://localhost:8081")
-    @RequestMapping("/upload")
-    public String handleSigUpload(@RequestParam("imageData") MultipartFile imageData) {
+    @PostMapping("/upload")
+    public String handleSigUpload(@RequestParam("imageDataUrl") String imageDataUrl) {
 
-        System.out.println("111");
-        if (imageData.isEmpty()) {
+        System.out.println(imageDataUrl);
+        if (imageDataUrl.isEmpty()) {
             return "上传失败，请选择签名图片";
         }
 
+        // 得到当前时间戳 格式化时间戳为字符串
+        long timeStamp = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String formattedTimeStamp = sdf.format(new Date(timeStamp));
+
+        // yyyyMMddHHmmss
+        String sig_name = formattedTimeStamp;
+        // path/yyyyMMddHHmmss.png
+        String sig_path = UPLOAD_PATH + "/" + formattedTimeStamp + ".png";
+
         try {
-//            // 获取文件名并构建文件保存路径
-//            String imageName = file.getOriginalFilename();   //xxx.xxx
-//            String[] parts = fileName.split("\\.");
-//            String name = parts[0]; // 文件名
-//            String doc_type = parts[1]; // 后缀名
-
-            // 得到当前时间戳 格式化时间戳为字符串
-            long timeStamp = System.currentTimeMillis();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-            String formattedTimeStamp = sdf.format(new Date(timeStamp));
-
-            // xxx_date
-            String sig_name = formattedTimeStamp;
-            System.out.println("sig name: " + sig_name);
-            // path/xxx_date.xxx
-            String sig_path = UPLOAD_PATH + "/" + formattedTimeStamp + ".png";
-            System.out.println("sig path: " + sig_path);
-
             // 插入数据库
             // user_id 还没写
             // id sig_id sig_name sig_path create_time user_id is_deleted
             sigService.uploadSigs(sig_name, sig_path, timeStamp);
 
-            // 将文件保存到指定路径
-            File dest = new File(sig_path);
-            imageData.transferTo(dest);
-
+            // 下载图像数据并保存到指定路径
+            String[] parts = imageDataUrl.split(",");
+            byte[] imageBytes = Base64.getDecoder().decode(parts[1]);
+            try (FileOutputStream fos = new FileOutputStream(sig_path)) {
+                fos.write(imageBytes);
+            }
             return "签名上传成功";
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            return "上传失败：" + e.getMessage();
+            return "签名上传失败";
         }
     }
 
