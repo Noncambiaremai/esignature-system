9c9ef504d50a879ffac33551629f1e8f553fa77b
wjiaying <wjiaying@neusoft.com>
2024-04-08 21:15:57 +0800

    前端添加了添加签名的dialog

--- vue_sys/src/components/mydoc.vue
+++ vue_sys/src/components/mydoc.vue
@@ -52,16 +52,17 @@
           <template slot-scope="scope">
             <div class="button-group">
             <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">添加签名</el-button>
-            <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">预览</el-button>
-            </div>
-            <div class="button-group-single">
-              <el-button
-                size="mini"
-                @click="handleEdit(scope.$index, scope.row)">下载</el-button>
+              size="mini" type="success" @click="dialogTableVisible = true">添加签名</el-button>
+              <!--@click="handleEdit(scope.$index, scope.row)"-->
+
+            <!--<el-button-->
+              <!--size="mini"-->
+              <!--@click="handleEdit(scope.$index, scope.row)">预览</el-button>-->
+            <!--</div>-->
+            <!--<div class="button-group-single">-->
+              <!--<el-button-->
+                <!--size="mini"-->
+                <!--@click="handleEdit(scope.$index, scope.row)">下载</el-button>-->
               <el-button
                 size="mini"
                 type="danger"
@@ -70,6 +71,41 @@
           </template>
         </el-table-column>
       </el-table>
+
+      <el-dialog :visible.sync="dialogTableVisible">
+        <el-table :data="sigTableData">
+          <el-table-column label="签名图片" style="width: 20%">
+            <template slot-scope="scope">
+              <img :src="scope.row.image" style="width: 50%; height: 25%;" alt="图片">
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="签名创建时间"
+            style="width: 20%">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ new Date(scope.row.create_time).toLocaleString()}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="签名名称"
+            style="width: 20%">
+            <template slot-scope="scope">
+              <div slot="reference" class="name-wrapper">
+                <el-tag size="medium">{{ scope.row.sig_name }}</el-tag>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" style="width: 20%">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="success"
+                @click="handleChoose(scope.$index, scope.row)">选择</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-dialog>
     </Menu>
   </div>
 </template>
@@ -84,11 +120,22 @@ import Menu from "@/components/menu.vue";
         },
         data() {
             return {
-              tableData: []
+              tableData: [],
+              sigTableData: [{
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+              }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+              }],
+              dialogTableVisible: false,
             }
         },
         created() {
             this.selectFileList();
+            this.selectSigList();
         },
         methods: {
           selectFileList() {
@@ -98,6 +145,17 @@ import Menu from "@/components/menu.vue";
             }).catch(error => {
               console.error('Failed to fetch file list:', error);
             });
+          },
+          selectSigList() {
+            axios.get('/api/sig/selectAllByUserId').then(response => {
+              this.sigTableData = response.data;
+              console.log(this.sigTableData);
+            }).catch(error => {
+              console.error('Failed to fetch signature list:', error);
+            });
+          },
+          handleChoose(index, row) {
+            console.log(row);
           },
           handleDelete(index, row) {
             this.$confirm('确定删除该文件?', '提示', {
