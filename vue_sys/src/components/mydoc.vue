cf8623ce4f6f30682e4c343384091c8db3f4e3de
wjiaying <wjiaying@neusoft.com>
2024-04-11 20:11:10 +0800

    前端将我的文件页面部分功能转到合成区域页面
    代码转移到另一个页面

--- vue_sys/src/components/mydoc.vue
+++ vue_sys/src/components/mydoc.vue
@@ -47,8 +47,6 @@
         <el-table-column label="操作" style="width: 30%">
           <template slot-scope="scope">
             <div class="button-group">
-            <el-button
-              size="mini" type="success" @click="dialogTableVisible = true">添加签名</el-button>
               <!--@click="handleEdit(scope.$index, scope.row)"-->
 
             <!--<el-button-->
@@ -67,54 +65,6 @@
           </template>
         </el-table-column>
       </el-table>
-
-      <!--点击添加签名之后的dialog-->
-      <el-dialog :visible.sync="dialogTableVisible">
-        <el-table :data="sigTableData">
-          <el-table-column label="签名图片" style="width: 20%">
-            <template slot-scope="scope">
-              <img :src="scope.row.image" style="width: 50%; height: 25%;" alt="图片">
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="签名创建时间"
-            style="width: 20%">
-            <template slot-scope="scope">
-              <i class="el-icon-time"></i>
-              <span style="margin-left: 10px">{{ new Date(scope.row.create_time).toLocaleString()}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="签名名称"
-            style="width: 20%">
-            <template slot-scope="scope">
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.sig_name }}</el-tag>
-              </div>
-            </template>
-          </el-table-column>
-          <el-table-column label="操作" style="width: 20%">
-            <template slot-scope="scope">
-              <el-button
-                size="mini"
-                type="success"
-                @click="handleChoose(scope.$index, scope.row)">选择</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-dialog>
-
-      <!--PDF部分-->
-      <el-dialog :visible.sync="previewPDFDialog">
-        <div>
-          <el-button :theme="'default'" type="submit" :title="'上一页'" @click.stop="prePage"> 上一页</el-button>
-          <el-button :theme="'default'" type="submit" :title="'下一页'" @click.stop="nextPage"> 下一页</el-button>
-          <div class="page">{{pageNum}}/{{pageTotalNum}} </div>
-        </div>
-        <PDF :src="url" :page="pageNum" @progress="loadedRatio = $event" @page-loaded="pageLoaded($event)"
-             @num-pages="pageTotalNum=$event" @error="pdfError($event)"  @link-clicked="page = $event">
-        </PDF>
-      </el-dialog>
     </Menu>
   </div>
 </template>
@@ -132,52 +82,18 @@ import PDF from 'vue-pdf';
         data() {
             return {
               tableData: [],
-              sigTableData: [{
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-              }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-              }],
-              dialogTableVisible: false,
-
-              // 有关PDF的data
-              previewPDFDialog:false,
-              url: "http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf",
-              pageNum: 1,
-              pageTotalNum: 1,
-              // 加载进度
-              loadedRatio: 0,
-              curPageNum: 0,
             }
         },
         created() {
             this.selectFileList();
-            this.selectSigList();
         },
         methods: {
           selectFileList() {
             axios.get('/api/doc/selectAllByUserId').then(response => {
               this.tableData = response.data;
-              console.log(this.tableData);
             }).catch(error => {
               console.error('Failed to fetch file list:', error);
             });
-          },
-          selectSigList() {
-            axios.get('/api/sig/selectAllByUserId').then(response => {
-              this.sigTableData = response.data;
-              console.log(this.sigTableData);
-            }).catch(error => {
-              console.error('Failed to fetch signature list:', error);
-            });
-          },
-          handleChoose(index, row) {
-            console.log(row);
-            this.dialogTableVisible = false;
-            this.previewPDFDialog = true;
           },
           handleDelete(index, row) {
             this.$confirm('确定删除该文件?', '提示', {
@@ -192,27 +108,6 @@ import PDF from 'vue-pdf';
               this.$message({ type: 'success', message: '删除成功!' });
             }).catch(() => { this.$message({ type: 'info', message: '已取消删除' });
             });
-          },
-
-          // 上一页函数，
-          prePage() {
-            var page = this.pageNum
-            page = page > 1 ? page - 1 : this.pageTotalNum
-            this.pageNum = page
-          },
-          // 下一页函数
-          nextPage() {
-            var page = this.pageNum
-            page = page < this.pageTotalNum ? page + 1 : 1
-            this.pageNum = page
-          },
-          // 页面加载回调函数，其中e为当前页数
-          pageLoaded(e) {
-            this.curPageNum = e
-          },
-          // 错误时回调函数。
-          pdfError(error) {
-            console.error(error)
           },
         }
     }
