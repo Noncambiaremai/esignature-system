0748b19e24bd0cdeedf62346adc3d741670a53fa
wjiaying <2272766069@qq.com>
2024-04-14 19:59:40 +0800

    前端修改合成区域页面实现pdf入canvas
    把本来用的vue-pdf改成使用test界面的pdf.js 直接用canvas

--- vue_sys/src/components/mergePage.vue
+++ vue_sys/src/components/mergePage.vue
@@ -3,24 +3,19 @@
       <Menu>
         <div style="display: flex;">
           <!-- 左侧容器 -->
-          <div style="width: 33%; margin-right: 15px;">
+          <div style="width: 45%; margin-right: 15px;">
             <div style="display: flex;  flex-direction: column;">
-
               <div style="display: flex;">
                 <el-button round style="width: 200px; height: 70px; font-size: 19px; font-family: PingFang SC; margin-bottom: 10px;"
                                             @click="startChooseSig">选择签名</el-button>
                 <el-button round style="width: 200px; height: 70px; font-size: 19px; font-family: PingFang SC; margin-bottom: 10px;"
                            @click="startChooseFile">选择文件</el-button>
-
-
-
               </div>
-
               <div style="display: flex;">
                 <el-button  round style="width: 200px; height: 70px; font-size: 19px; font-family: PingFang SC; margin-bottom: 10px;"
-                  :title="'上一页'" @click.stop="prePage"> 上一页</el-button>
+                  :title="'上一页'" @click="clickPre"> 上一页</el-button>
                 <el-button  round style="width: 200px; height: 70px; font-size: 19px; font-family: PingFang SC; margin-bottom: 10px;"
-                  :title="'下一页'" @click.stop="nextPage"> 下一页</el-button>
+                  :title="'下一页'" @click="clickNext"> 下一页</el-button>
               </div>
               <div style="overflow: auto;  border: 2px solid #ccc; height: 100px">
                 <img v-show="imageData!=''" :src="imageData" alt="图片" style="max-width: 100px; max-height: 100px;">
@@ -29,11 +24,13 @@
           </div>
 
           <!-- 右侧容器 -->
-          <div style="width: 66.666%; overflow: auto;  border: 2px solid #ccc; height: 730px">
-            <div class="page">{{pageNum}}/{{pageTotalNum}} </div>
-            <PDF :src="pdfData" :page="pageNum" @progress="loadedRatio = $event" @page-loaded="pageLoaded($event)"
-                 @num-pages="pageTotalNum=$event" @error="pdfError($event)"  @link-clicked="page = $event">
-            </PDF>
+          <!--<div style="width: 66.666%; overflow: auto;  border: 2px solid #ccc; height: 730px">-->
+          <div ref="canvasCont" class="canvas-container">
+          <div class="page">{{pageNo}}/{{pdfPageNumber}} </div>
+            <!--<PDF :src="pdfData" :page="pageNum" @progress="loadedRatio = $event" @page-loaded="pageLoaded($event)"-->
+                 <!--@num-pages="pageTotalNum=$event" @error="pdfError($event)"  @link-clicked="page = $event">-->
+            <!--</PDF>-->
+              <canvas ref="myCanvas" class="pdf-container"></canvas>
           </div>
         </div>
 
@@ -111,20 +108,22 @@
             </el-table-column>
           </el-table>
         </el-dialog>
-
       </Menu>
     </div>
 </template>
 
 <script>
   import axios from 'axios';
+  // import fabric from 'fabric';
   import Menu from "@/components/menu.vue";
-  import PDF from 'vue-pdf';
+  // import PDF from 'vue-pdf';
+  const pdfJS = require("pdfjs-dist");
+
+  pdfJS.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry");
     export default {
         name: "mergePage",
         components: {
           Menu,
-          PDF,
         },
       data() {
           return {
@@ -136,13 +135,15 @@
 
             // 有关PDF的data
             previewPDFDialog:false,
-            pdfData: "",
-            pageNum: 1,
-            pageTotalNum: 1,
-            // 加载进度
-            loadedRatio: 0,
-            curPageNum: 0,
-            url: "http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf",
+            pageNo: null, // 当前页
+            pdfPageNumber: null,  // 总页数
+            renderingPage: false,
+            pdfData: null, // PDF的base64
+            scale: null, // 缩放值
+            width: "",
+            height: "",
+            pdfbase64: "",
+            // url:"http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf",
           }
       },
       created() {
@@ -179,46 +180,124 @@
         chooseSig(index, row) {
           this.sigdialogTableVisible = false;
           this.imageData = row.image;
-          // console.log(row.image);
-          // axios.get('/api/sig/selectFileByDocPath',
-          //   { params: { filePath: row.doc_path }}).then(response => {
-          //   // console.log(response.data.file);
-          //   // this.pdfData = response.data.file;
-          // });
         },
         chooseFile(index, row) {
           this.filedialogTableVisible = false;
           axios.get('/api/doc/selectFileByDocPath',
             { params: { filePath: row.doc_path }}).then(response => {
             console.log(response.data.file);
-            this.pdfData = response.data.file;
-          });
+            this.pdfbase64 = response.data.file;
 
+            // 引入pdf.js的字体
+            let CMAP_URL = "https://unpkg.com/pdfjs-dist@2.0.943/cmaps/";
+            //读取base64的pdf流文件
+            this.pdfData = pdfJS.getDocument({
+              data: atob(this.pdfbase64), // PDF base64编码
+              cMapUrl: CMAP_URL,
+              cMapPacked: true,
+            });
+            // 渲染
+            this.renderPage(1);
+          });
         },
-        // 上一页函数，
-        prePage() {
-          var page = this.pageNum
-          page = page > 1 ? page - 1 : this.pageTotalNum
-          this.pageNum = page
+
+        // 根据页码渲染相应的PDF
+        renderPage(num) {
+          this.renderingPage = true;
+          this.pdfData.promise.then((pdf) => {
+            // pdf的总页数
+            this.pdfPageNumber = pdf.numPages;
+
+            // 得到pdf具体的页
+            pdf.getPage(num).then((page) => {
+              // 获取DOM中为预览PDF准备好的canvasDOM对象
+              let canvas = this.$refs.myCanvas;
+              let ctx = canvas.getContext("2d");
+
+              // 根据页面宽度和视口宽度的比率就是内容区的放大比率
+              // dialogWidth表示装PDF的容器的宽度 也就是div的宽度
+              let dialogWidth = this.$refs["canvasCont"].offsetWidth;
+
+              // page.view[2]表示pdf页面原本的宽度
+              let pageWidth = page.view[2];
+              let scale = dialogWidth / pageWidth;
+              let viewport = page.getViewport({ scale });
+
+              // 记录内容区宽高，后期添加水印时需要
+              this.width = viewport.width;
+              this.height = viewport.height;
+              canvas.width = this.width;
+              canvas.height = this.height;
+
+              let renderContext = {
+                canvasContext: ctx,
+                viewport: viewport,
+              };
+              page.render(renderContext).promise.then(() => {
+                this.renderingPage = false;
+                this.pageNo = num;
+
+                // 添加图片
+                this._renderImage();
+              });
+            });
+          });
         },
-        // 下一页函数
-        nextPage() {
-          var page = this.pageNum
-          page = page < this.pageTotalNum ? page + 1 : 1
-          this.pageNum = page
+
+        // 在画布上渲染图片
+        _renderImage() {
+          let canvas = this.$refs.myCanvas;
+          let ctx = canvas.getContext("2d");
+
+          // 创建一个新的Image对象
+          let image = new Image();
+          // 设置图片的src为你想要添加的图片的 base64 编码字符串
+          // image.src = "";
+          image.src = this.imageData;
+          // 等待图片加载完成
+          image.onload = () => {
+            // 在画布上绘制图片
+            // ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+            ctx.drawImage(image, 0, 0, image.width / 3, image.height / 3);
+          };
         },
-        // 页面加载回调函数，其中e为当前页数
-        pageLoaded(e) {
-          this.curPageNum = e
+
+        clickPre() {
+          // 当前页面未被渲染 && 还有上一页
+          if (!this.renderingPage && this.pageNo && this.pageNo > 1) {
+            this.renderPage(this.pageNo - 1); // 渲染上一页
+          }
+          //   var page = this.pageNum
+          //   page = page > 1 ? page - 1 : this.pageTotalNum
+          //   this.pageNum = page
         },
-        // 错误时回调函数。
-        pdfError(error) {
-          console.error(error)
+        clickNext() {
+          // 当前页面未被渲染 && 还有下一页
+          if (
+            !this.renderingPage && this.pdfPageNumber &&
+            this.pageNo && this.pageNo < this.pdfPageNumber
+          ) {
+            this.renderPage(this.pageNo + 1); // 渲染下一页
+          }
+          //   var page = this.pageNum
+          //   page = page < this.pageTotalNum ? page + 1 : 1
+          //   this.pageNum = page
         },
       }
     }
 </script>
 
 <style scoped>
-
+  .canvas-container {
+    width: 600px;
+    height: 790px;
+    border: 1px dashed black;
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+  .pdf-container {
+    width: 100%;
+    height: 100%;
+  }
 </style>
