29110111377dfb29a1d84f1d3b89a890c7df6005
wjiaying <2272766069@qq.com>
2024-04-21 20:05:01 +0800

    找出了所需要的人脸关键点

--- vue_sys/src/components/enroll.vue
+++ vue_sys/src/components/enroll.vue
@@ -99,16 +99,42 @@
 
         if (results.multiFaceLandmarks) {
           for (const landmarks of results.multiFaceLandmarks) {
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_TESSELATION,
-              {color: '#C0C0C070', lineWidth: 1});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_TESSELATION,
+            //   {color: '#C0C0C070', lineWidth: 1});
+            // console.log(FACEMESH_FACE_OVAL);
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+            // // drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+            // // drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
             // drawConnectors(this.canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
-            drawConnectors(this.canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+            // drawConnectors(this.canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+
+            // 要绘制的关键点列表
+            const landmarksList = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291,
+              185, 40, 39, 37, 0, 267, 269, 270, 409, 78, 95, 88, 178, 87, 14, 317,
+              402, 318, 324, 308, 191, 80, 81, 82, 13, 312, 311, 310, 415, 263, 249,
+              390, 373, 374, 380, 381, 382, 362, 466, 388, 387, 386, 385, 384, 398,
+              276, 283, 282, 295, 285, 300, 293, 334, 296, 336, 33, 7, 163, 144, 145,
+              153, 154, 155, 133, 246, 161, 160, 159, 158, 157, 173, 46, 53, 52, 65,
+              55, 70, 63, 105, 66, 107, 10, 338, 297, 332, 284, 251, 389, 356, 454,
+              323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150,
+              136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109, 168, 6, 197,
+              195, 5, 4, 1, 19, 94, 2, 98, 97, 326, 327, 294, 278, 344, 440, 275, 45,
+              220, 115, 48, 64];
+
+              // 绘制关键点
+            for (let i = 0; i < landmarksList.length; i++) {
+              const index = landmarksList[i];
+              const x = landmarks[index].x * this.canvasElement.width; // 将归一化的水平坐标转换为画布坐标
+              const y = landmarks[index].y * this.canvasElement.height; // 将归一化的垂直坐标转换为画布坐标
+              this.canvasCtx.beginPath();
+              this.canvasCtx.arc(x, y, 2, 0, 2 * Math.PI); // 画一个圆
+              this.canvasCtx.fillStyle = '#6bff25';
+              this.canvasCtx.fill();
+              this.canvasCtx.closePath();
+            }
           }
           this.tempFaceFeature = results.multiFaceLandmarks[0];
           // console.log(this.faceFeature);
@@ -136,7 +162,9 @@
 
       // 拍照录入人脸
       takeImage() {
+
         this.faceFeature = this.tempFaceFeature;
+        console.log(this.faceFeature);
         // 把当前画布上的人脸图片截下来保存到一个变量中 this.faceImage
         // this.faceImage和this.faceFeature里面都有数据 关闭摄像头
         if (!this.canvasElement) {
