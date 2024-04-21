dc192b1ead39eb54d4efd0046ac4748d6447323c
wjiaying <2272766069@qq.com>
2024-04-21 16:24:08 +0800

    删掉一些没用的

--- vue_sys/src/components/menu.vue
+++ vue_sys/src/components/menu.vue
@@ -12,22 +12,6 @@
               <el-menu-item index="1-5" @click="gotoMyDoc"><i class="el-icon-notebook-2"></i>我的文件</el-menu-item>
               <el-menu-item index="1-6" @click="gotoMergePage"><i class="el-icon-connection"></i>合成区域</el-menu-item>
               <el-menu-item index="1-7" @click="gotoRecord"><i class="el-icon-time"></i>签名日志</el-menu-item>
-
-          <!--<el-submenu index="2">-->
-            <!--<template slot="title"><i class="el-icon-menu"></i>导航二</template>-->
-            <!--<el-menu-item-group>-->
-              <!--<template slot="title">分组一</template>-->
-              <!--<el-menu-item index="2-1">选项1</el-menu-item>-->
-              <!--<el-menu-item index="2-2">选项2</el-menu-item>-->
-            <!--</el-menu-item-group>-->
-            <!--<el-menu-item-group title="分组2">-->
-              <!--<el-menu-item index="2-3">选项3</el-menu-item>-->
-            <!--</el-menu-item-group>-->
-            <!--<el-submenu index="2-4">-->
-              <!--<template slot="title">选项4</template>-->
-              <!--<el-menu-item index="2-4-1">选项4-1</el-menu-item>-->
-            <!--</el-submenu>-->
-          <!--</el-submenu>-->
         </el-menu>
       </el-aside>
 
